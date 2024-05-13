c8d3ff96b94dfce7a84a42f74ac97d8bf33c78dd
Spiros Ioannou <sivann@inaccess.com>
2024-05-13 16:31:31 +0300

    prometheus instrumentation

--- jobrunner.go
+++ jobrunner.go
@@ -7,15 +7,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
+	"sync/atomic"
 	"time"
-    "log/slog"
-    "sync/atomic"
 
-	// "io/ioutil"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type JobRequest struct {
@@ -34,8 +35,14 @@ type JobResult struct {
 }
 
 type StatusResponse struct {
-	QueuedJobs int `json:"queued_jobs"`
-	TotalJobs uint64 `json:"total_jobs"`
+	QueuedJobs int    `json:"queued_jobs"`
+	TotalJobs  uint64 `json:"total_jobs"`
+}
+
+type metrics struct {
+	QueuedJobs  prometheus.Gauge
+	TotalJobs   *prometheus.CounterVec
+	JobDuration *prometheus.SummaryVec
 }
 
 // job payload includes the input request, and a channel to wait for the worker to write the result to
@@ -49,7 +56,7 @@ var (
 	JobQueueCap       = NumWorkers * 10
 	HttpListenAddress = "127.0.0.1"
 	HttpListenPort    = 8080
-    TotalJobs         = uint64(0)
+	TotalJobs         = uint64(0)
 )
 
 func exitCode(err error) int {
@@ -60,18 +67,45 @@ func exitCode(err error) int {
 }
 
 func Infof(format string, args ...any) {
-    slog.Default().Info(fmt.Sprintf(format, args...))
+	slog.Default().Info(fmt.Sprintf(format, args...))
+}
+
+func PrometheusMetrics(reg prometheus.Registerer) *metrics {
+	m := &metrics{
+		QueuedJobs: prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "queued_jobs",
+			Help: "Jobs queued in worker queue.",
+		}),
+		TotalJobs: prometheus.NewCounterVec(
+			prometheus.CounterOpts{
+				Name: "total_jobs",
+				Help: "Number of jobs started, per worker.",
+			},
+			[]string{"worker_id"}, //labels
+		),
+		JobDuration: prometheus.NewSummaryVec(
+			prometheus.SummaryOpts{
+				Name: "job_duration",
+				Help: "Job duration in float seconds, per worker.",
+			},
+			[]string{"worker_id"}, //labels
+		),
+	}
+	reg.MustRegister(m.QueuedJobs)
+	reg.MustRegister(m.TotalJobs)
+	reg.MustRegister(m.JobDuration)
+	return m
 }
 
 func ExecuteCommand(wid int, cmd1 *exec.Cmd) ([]byte, int, []byte) {
 	jr_cmd := os.Getenv("JOBRUNNER_CMD")
-	slog.Info("worker","JOBRUNNE_CMD", jr_cmd)
+	slog.Info("worker", "JOBRUNNE_CMD", jr_cmd)
 
 	cmd := exec.Command(jr_cmd)
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "JOBRUNNER_WORKER_ID="+strconv.Itoa(wid))
 
-	slog.Info("ExecuteCommand","workerId",wid, "command",jr_cmd)
+	slog.Info("ExecuteCommand", "workerId", wid, "command", jr_cmd)
 
 	var outb, errb bytes.Buffer
 	cmd.Stdout = &outb
@@ -89,20 +123,22 @@ func ExecuteCommand(wid int, cmd1 *exec.Cmd) ([]byte, int, []byte) {
 	return data, exitStatus, cmdErr
 }
 
-func worker(wid int, jobs chan JobPayload) {
+func worker(wid int, jobs chan JobPayload, m metrics) {
 	cmd := exec.Command("ls")
 	for {
 		jpl := <-jobs
-		slog.Info("worker: received job","JobPayload",jpl)
-        atomic.AddUint64(&TotalJobs,1)
+		slog.Info("worker: received job", "JobPayload", jpl)
+		atomic.AddUint64(&TotalJobs, 1)
 
 		start := time.Now()
 		result_data, result_status, result_message := ExecuteCommand(wid, cmd)
 		elapsed := time.Since(start)
+		m.JobDuration.WithLabelValues(strconv.Itoa(wid)).Observe(float64(elapsed))
+		m.TotalJobs.WithLabelValues(strconv.Itoa(wid)).Inc()
 
 		jres := JobResult{Data: result_data, ExitStatus: result_status, Message: result_message, ElapsedSec: elapsed.Seconds(), Wid: wid}
 
-		slog.Info("worker returning data to Jobready channel.","wid", wid, "result_length",len(result_data))
+		slog.Info("worker returning data to Jobready channel.", "wid", wid, "result_length", len(result_data))
 		jpl.JobReady <- jres // return result to payload handler
 	}
 }
@@ -121,7 +157,7 @@ func payloadHandler(jobs chan JobPayload) http.HandlerFunc {
 		err := decoder.Decode(&jobreq)
 
 		if err != nil {
-			log.Println("payloadHandler: error decoding json")
+			slog.Error("payloadHandler: error decoding json", "error", err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
@@ -138,14 +174,14 @@ func payloadHandler(jobs chan JobPayload) http.HandlerFunc {
 		}
 		jobid, err := strconv.Atoi(jobreq.Id)
 		if err != nil {
-			log.Println("Job ID not numeric")
+			slog.Error("payloadHandler: Job ID not numeric", "error", err)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("Job ID not numeric\n"))
 			return
 		}
 
 		if len(jobs) >= JobQueueCap {
-			Infof("payloadHandler: denying request for job %d, job queue too large\n", jobid)
+			slog.Error("payloadHandler: denying request, job queue too large.", "queue_length", len(jobs), "jobid", jobid)
 			w.WriteHeader(http.StatusServiceUnavailable)
 			fmt.Fprintf(w, "job queue too large (%d)\n", len(jobs))
 			return
@@ -187,48 +223,57 @@ func statusHandler(jobs chan JobPayload) http.HandlerFunc {
 }
 
 func main() {
+
+	//prometheus registry
+	preg := prometheus.NewRegistry()
+	//register metrics
+	m := PrometheusMetrics(preg)
+
+	//jobs channel to send jobs requests from http handlers to workers
 	jobs := make(chan JobPayload, JobQueueCap)
 
-    jnw, err := strconv.Atoi(os.Getenv("JOBRUNNER_NUM_WORKERS"))
-    if err == nil {
-        NumWorkers = jnw
-		slog.Info("main: setting NumWorkers from JOBRUNNER_NUM_WORKERS env variable." ,"NumWorkers",NumWorkers)
-    }
+	jnw, err := strconv.Atoi(os.Getenv("JOBRUNNER_NUM_WORKERS"))
+	if err == nil {
+		NumWorkers = jnw
+		slog.Info("main: setting NumWorkers from JOBRUNNER_NUM_WORKERS env variable.", "NumWorkers", NumWorkers)
+	}
 
-    //validate JOBRUNNER_CMD
-    jc := os.Getenv("JOBRUNNER_CMD")
-    if len(jc) == 0 {
-        fmt.Fprintf(os.Stderr, "JOBRUNNER_CMD is empty\n")
-        os.Exit(1)
-    }
-    path, err := exec.LookPath(jc)
+	//validate JOBRUNNER_CMD
+	jc := os.Getenv("JOBRUNNER_CMD")
+	if len(jc) == 0 {
+		fmt.Fprintf(os.Stderr, "JOBRUNNER_CMD is empty\n")
+		os.Exit(1)
+	}
+	path, err := exec.LookPath(jc)
 	if err != nil {
 		log.Fatal("Cannot find command specified in JOBRUNNER_CMD env var:", jc)
 	}
 	Infof("Path for JOBRUNNER_CMD: %s\n", path)
 
-
 	//start worker
 	for w := 1; w <= NumWorkers; w++ {
 		slog.Info("main: starting worker", "workerID", w)
-		go worker(w, jobs)
+		go worker(w, jobs, *m)
 	}
 
 	go func() {
 		http.HandleFunc("/payload", payloadHandler(jobs))
 		http.HandleFunc("/status", statusHandler(jobs))
+		http.Handle("/metrics", promhttp.HandlerFor(preg, promhttp.HandlerOpts{Registry: preg}))
+
 		err := http.ListenAndServe(HttpListenAddress+":"+strconv.Itoa(HttpListenPort), nil)
 		if err != nil {
-			log.Println("Starting listening for payload messages on port:", HttpListenPort)
+			slog.Info("Starting listening for payload messages.", "port", HttpListenPort)
 		} else {
-			log.Fatalf("An error occured while starting payload server %v", err.Error())
+			slog.Error("An error occured while starting payload server", "error", err.Error())
 		}
 	}()
 
-    //loop and log queue length
+	//loop and log queue length
 	for {
-		time.Sleep(time.Duration(15000) * time.Millisecond)
-		slog.Info("main loop", "queue_length", len(jobs))
+		time.Sleep(time.Duration(2000) * time.Millisecond)
+		m.QueuedJobs.Set(float64(len(jobs)))
+		//slog.Info("main loop", "queue_length", len(jobs))
 	}
 
 }
